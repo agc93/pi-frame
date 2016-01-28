176186030a135b8ac128918af308c35eba99c763
Alistair Chapman <alistairgchapman@gmail.com>
2016-01-29 00:49:41 +1000

    I can't do of the Python

--- pi-frame-web/runserver.py
+++ pi-frame-web/runserver.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
         PORT = int(environ.get('SERVER_PORT', '5555'))
     except ValueError:
         PORT = 5555
-    var debugMode = environ.get('APP_DEBUG', False)
+    debugMode = environ.get('APP_DEBUG', False)
     if debugMode == True:
         app.debug = True
         app.config['SECRET_KEY'] = '0123456789'
